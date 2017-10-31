4cd7692613859c19d64405e396ff6f47d2fbd9d5
Raymond Sutton <raymond.sutton.15@cnu.edu>
2017-10-31 14:22:08 -0400

    Add files via upload

---
+++ SELECT Statements/select_statement_exercise.sql
@@ -0,0 +1,20 @@
+SELECT TOP 20 PERCENT NationalIDNumber AS [SSN], JobTitle AS [Job Title], BirthDate
+FROM HumanResources.Employee
+
+SELECT TOP 50 PERCENT *
+FROM Sales.Customer
+
+SELECT Name AS [Product's Name]
+FROM Production.vProductAndDescription
+
+SELECT TOP 400 *
+FROM HumanResources.Department
+
+SELECT *
+FROM Production.BillOfMaterials
+
+SELECT TOP 1500 *
+FROM Sales.vPersonDemographics
+
+
+
---
+++ SELECT Statements/using_table_column_name_aliases.sql
@@ -0,0 +1,3 @@
+SELECT TOP 100 
+	FirstName AS [Customer First Name], LastName AS "Customer Last Name"
+FROM Person.Person

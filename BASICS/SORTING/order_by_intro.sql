41e087b259c6dedd1503c30ec2180ca7db18f6db
Raymond Sutton <raymond.sutton.15@cnu.edu>
2017-10-31 15:41:53 -0400

    Time to sort data gathered from a database

---
+++ BASICS/SORTING/order_by_intro.sql
@@ -0,0 +1,7 @@
+SELECT FirstName, LastName
+FROM Sales.vIndividualCustomer
+ORDER BY FirstName ASC 
+
+SELECT FirstName, LastName AS [Customer Last Name]
+FROM Sales.vIndividualCustomer
+ORDER BY [Customer Last Name] ASC 

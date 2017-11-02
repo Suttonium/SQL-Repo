1740472df0a04f8836c5c32d2822ed66d928e908
Raymond Sutton <raymond.sutton.15@cnu.edu>
2017-11-02 10:21:41 -0400

    Delete order_by_intro.sql

--- BASICS/SORTING/order_by_intro.sql
+++
@@ -1,7 +0,0 @@
-SELECT FirstName, LastName
-FROM Sales.vIndividualCustomer
-ORDER BY FirstName ASC 
-
-SELECT FirstName, LastName AS [Customer Last Name]
-FROM Sales.vIndividualCustomer
-ORDER BY [Customer Last Name] ASC 
